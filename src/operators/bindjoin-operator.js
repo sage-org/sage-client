57ef487fcd1c779bf58b273b46031a711200b7b8
Slaanaroth <corentin.marionneau@gmail.com>
2018-07-25 14:07:55 +0200

    Sacred Lodash fire purified bindjoin heresy

--- src/operators/bindjoin-operator.js
+++ src/operators/bindjoin-operator.js
@@ -63,7 +63,7 @@ class BindJoinOperator extends BufferedIterator {
 
           if (mapping != null) {
             var cpt = that._bucket.length;
-            var bind = Object.assign({}, that._bgp)
+            var bind = _.cloneDeep(that._bgp)
             for (var i = 0; i < bind.length; i++) {
               for (var v in mapping) {
                 var tp = bind[i];
