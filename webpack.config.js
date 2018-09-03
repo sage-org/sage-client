fd2e42ae1cf2b476b18b85265d2c16e4537dad5e
Thomas Minier <tminier01@gmail.com>
2018-09-03 11:27:41 +0200

    fix issue with webpack bundles

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules(?!\/sparql-engine)/,
         use: {
           loader: 'babel-loader',
           options: {
